506f06c3c54f40e6b230e283a9592340b353711e
jiminnote <trace_of_jimin@naver.com>
2025-07-10 21:56:27 +0900

    Update : 주석처리

--- auto_file_sorter_project/filedays.py
+++ auto_file_sorter_project/filedays.py
@@ -4,36 +4,32 @@ import os
 import shutil
 from datetime import datetime
 
-from project.filetidy_project.collect_png_images import collect_screenshots
-
-
+# 파일의 수정일 얻어오는 함수
 def get_file_date(path):
     t = os.path.getmtime(path)
-    return datetime.fromtimestamp(t).strftime('%Y-%m-%d')
+    return datetime.fromtimestamp(t).strftime('%Y-%m-%d') # 파일의 마지막 수정시간을 초단위(fromtimestamp)로 가져옴
 
+# 수정일 기준 + 확장자 폴더로 정리하는 함수
 def move_files_date(src_path, dest_path):
-    for file in os.listdir(src_path):
-        file_path = os.path.join(src_path, file)
-        if os.path.isdir(file_path):
+    for file in os.listdir(src_path): # 원본 폴더에 있는 모든 항목에 대해 반복
+        file_path = os.path.join(src_path, file) # 전체 파일 경로 생성
+        if os.path.isdir(file_path): # 
             try:
-                mod_date = get_file_date(file_path)
-                dest_folder = os.path.join(dest_path, mod_date)
+                mod_date = get_file_date(file_path) # 해당 항목의 수정일 구함
+                dest_folder = os.path.join(dest_path, mod_date) # 날짜별 폴더 생성 경로 구성
 
-                ext = file.split(".")[-1]
-                ext_folder = os.path.join(dest_folder, ext)
-                os.makedirs(ext_folder, exist_ok=True)
+                ext = file.split(".")[-1] # 파일의 확장자 추출
+                ext_folder = os.path.join(dest_folder, ext) # 날짜폴더 안에 확장자별 하위 폴더 경로 구성 (예: ./screenshots/2025-07-10/png)
+                os.makedirs(ext_folder, exist_ok=True) # 폴더가 없으면 새로 만들고, 있으면 유지
 
-                shutil.move(file_path, os.path.join(ext_folder, file))
-                print(f"{file}->{mod_date}/{ext}/")
+                shutil.move(file_path, os.path.join(ext_folder, file)) # 해당 파일을 새 폴더로 이동
+                print(f"{file}->{mod_date}/{ext}/") # 로그 출력
             except Exception as e:
                 print(e)
 
 def main() :
     src = '/Users/jimin/Desktop'
     dest = './screenshots'
-
-    collect_screenshots(src, dest)
-    print("SUCCESS!!")
 
     if not os.path.exists(src):
         print("No Exist!!")
--- auto_file_sorter_project/filetidy.py
+++ auto_file_sorter_project/filetidy.py
@@ -1,24 +1,27 @@
 import os.path
-import shutil
+import shutil # 파일 이동용
 from datetime import datetime
 
+# 날짜 기반 폴더 생성 함수
 def create_date_folder(base_path):
-    today = datetime.today().strftime("%Y-%m-%d")
-    folder_path = os.path.join(base_path, today)
-    os.makedirs(folder_path, exist_ok=True)
+    today = datetime.today().strftime("%Y-%m-%d") # 오늘 날짜를 %Y-%m-%d 형식의 문자열로 저장
+    folder_path = os.path.join(base_path, today)  # base 폴더와 날짜를 붙여서 전체 경로 생성(./organized_files/2025-07-10)
+    os.makedirs(folder_path, exist_ok=True) # 해당 경로가 없으면 새로만듬(있으면 아무 일도 안일어남)
     return folder_path
 
+# 소스 폴더안에 있는 파일들만 가져오는 함수
 def get_file_list(src_path):
-    return [ f for f in os.listdir(src_path) if os.path.isfile(os.path.join(src_path, f))]
+    return [ f for f in os.listdir(src_path) if os.path.isfile(os.path.join(src_path, f))] # 전체 목록 중 파일만 필터링하여 리스트로 반환
 
+# 확장자 기준으로 서브 폴더 정리 함수
 def move_files_extension(src_path, dest_path):
-    files = get_file_list(src_path)
+    files = get_file_list(src_path) # 정리할 대상 파일 가져옴
     for file in files:
         try:
-            ext = file.split(".")[-1]
-            ext_folder = os.path.join(dest_path, file)
-            os.makedirs(ext_folder, exist_ok=True)
-            shutil.move(os.path.join(src_path, file), os.path.join(ext_folder, file))
+            ext = file.split(".")[-1] # 확장자 부분 가져옴(예: img.png → png)
+            ext_folder = os.path.join(dest_path, file) # (예: organized_files/2025-07-10/png)
+            os.makedirs(ext_folder, exist_ok=True) # 확장자 폴더 없으면 만듬
+            shutil.move(os.path.join(src_path, file), os.path.join(ext_folder, file)) # 소스path 에서  지정된 확장자 폴더로 이동
             print(f"{file}->{ext}/")
         except Exception as e:
             print(e)
